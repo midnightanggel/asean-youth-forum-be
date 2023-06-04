7a47a91e5ccd978b2ff89a87ebc1c636f8721853
raihanmr1311 <2024101010003@mail.unej.ac.id>
2023-06-04 23:10:02 +0700

    improvement: first commit

---
+++ models/articles.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const articleSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        trim: true,
+        required: [true, "Please add tittle"],
+    },
+    content:{
+        type: String,
+        trim: true,
+        required: [true, "please add content"],
+    },
+    image:{
+        type: String,
+
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+        required: [true],
+    },
+    like:{
+        type: Array,
+        default: [],
+    },
+    dislike:{
+        type: Array,
+        default: [],
+    },
+    comment:{
+        type: Array,
+        default: [],
+    },
+});
+
+export default mongoose.model("article", articleSchema);
