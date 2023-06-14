859e8d3cd5f5907bcc0d127b5a651df11f17c41c
sugiartofahmi <sugiartofahmi@gmail.com>
2023-06-14 15:43:27 +0700

    feature: comment

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,34 +1,40 @@
 const article = require("../models/articles.js");
+const Users = require("../models/users.js");
+
 const cloudinary = require("../config/cloudinary.js");
 // handle createarticle
-module.exports ={
-  createarticles : async (req, res) => {
+module.exports = {
+  createarticles: async (req, res) => {
     try {
-      const {title, content} = req.body;
-        if(!title){
-          return res.status(400).json({
-            status: "failed",
-            message: "Please add a Tittle",
-          })
-        }
-        if(!content){
-          return res.status(400).json({
-            status: "failed",
-            message: "Please add a Content" 
-          })
-        }
-      const _base64 = Buffer.from(req.files.image.data, 'base64').toString('base64');
-      const base64 = `data:image/jpeg;base64,${_base64}`;
-      const cloudinaryResponse = await cloudinary.uploader.upload(base64, { public_id: new Date().getTime() });
-      let articles = await article.create({
-        title : req.body.title,
-        content: req.body.content,
-        image : cloudinaryResponse.secure_url
-      })
-        res.status(200).json({
-          status: "success",
-          data: articles,
+      const { title, content } = req.body;
+      if (!title) {
+        return res.status(400).json({
+          status: "failed",
+          message: "Please add a Tittle",
         });
+      }
+      if (!content) {
+        return res.status(400).json({
+          status: "failed",
+          message: "Please add a Content",
+        });
+      }
+      const _base64 = Buffer.from(req.files.image.data, "base64").toString(
+        "base64"
+      );
+      const base64 = `data:image/jpeg;base64,${_base64}`;
+      const cloudinaryResponse = await cloudinary.uploader.upload(base64, {
+        public_id: new Date().getTime(),
+      });
+      let articles = await article.create({
+        title: req.body.title,
+        content: req.body.content,
+        image: cloudinaryResponse.secure_url,
+      });
+      res.status(200).json({
+        status: "success",
+        data: articles,
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({
@@ -37,10 +43,13 @@ module.exports ={
     }
   },
 
-//get all articles
-  getAllArticles : async (req, res) => {
+  //get all articles
+  getAllArticles: async (req, res) => {
     try {
-      const articles = await article.find();
+      const articles = await article
+        .find()
+        .populate("comments.user", "name")
+        .exec();
       if (!articles) {
         return res.status(404).json({ message: "Not found " });
       }
@@ -57,9 +66,12 @@ module.exports ={
   },
 
   //get article by id
-  getArticle : async (req, res) => {
+  getArticle: async (req, res) => {
     try {
-      const articles = await article.findById(req.params.id);
+      const articles = await article
+        .findById(req.params.id)
+        .populate("comments.user", "name")
+        .exec();
 
       if (!articles) {
         return res.status(404).json({ status: "Not found " });
@@ -71,39 +83,47 @@ module.exports ={
     } catch (error) {
       console.log(error);
       res.status(500).json({
-      status: "failed",
+        status: "failed",
       });
     }
   },
 
   //update article
-  updateArticle : async (req, res) => {
+  updateArticle: async (req, res) => {
     try {
-      const {title, content} = req.body;
-        if(!title){
-          return res.status(400).json({
-            status: "failed",
-            status: "Please add a Tittle",
-          })
-        }
-        if(!content){
-          return res.status(400).json({
-            status: "failed",
-            status: "Please add a Content" 
-          })
-        }
-      const _base64 = Buffer.from(req.files.image.data, 'base64').toString('base64');
+      const { title, content } = req.body;
+      if (!title) {
+        return res.status(400).json({
+          status: "failed",
+          status: "Please add a Tittle",
+        });
+      }
+      if (!content) {
+        return res.status(400).json({
+          status: "failed",
+          status: "Please add a Content",
+        });
+      }
+      const _base64 = Buffer.from(req.files.image.data, "base64").toString(
+        "base64"
+      );
       const base64 = `data:image/jpeg;base64,${_base64}`;
-  
-      const cloudinaryResponse = await cloudinary.uploader.upload(base64, { public_id: new Date().getTime() });
-      let articles = await article.findByIdAndUpdate(req.params.id, {
-        title : req.body.title,
-        content: req.body.content,
-        image : cloudinaryResponse.secure_url
-      }, {
-        new: true,
-        runValidators: true,
+
+      const cloudinaryResponse = await cloudinary.uploader.upload(base64, {
+        public_id: new Date().getTime(),
       });
+      let articles = await article.findByIdAndUpdate(
+        req.params.id,
+        {
+          title: req.body.title,
+          content: req.body.content,
+          image: cloudinaryResponse.secure_url,
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
       if (!articles) {
         return res.status(404).json({ status: "Article Not found" });
       }
@@ -114,26 +134,60 @@ module.exports ={
     } catch (error) {
       console.log(error);
       res.status(500).json({
-      status: "failed",
+        status: "failed",
       });
-  }
+    }
   },
-
-  deleteArticle : async (req, res) => {
+  //delete article
+  deleteArticle: async (req, res) => {
     try {
       const articles = await article.findByIdAndRemove(req.params.id);
       if (!articles) {
         return res.status(404).json({ status: "Not found " });
       }
       res.status(200).json({
-        status: "Article has been deleted",
+        status: "success",
+        message: "Article has been deleted",
         data: articles,
       });
     } catch (error) {
       console.log(error);
       res.status(500).json({
-      status: "failed",
+        status: "failed",
       });
-  }
-  }
-}
+    }
+  },
+
+  //add comment on article
+  createComment: async (req, res) => {
+    try {
+      const articles = await article.findById(req.params.id);
+      if (!articles) {
+        return res
+          .status(404)
+          .json({ status: "failed", message: "Article not found" });
+      }
+      const user = await Users.findById(req.user.id);
+      if (!user) {
+        return res
+          .status(404)
+          .json({ status: "failed", message: "User not found" });
+      }
+      articles.comments.push({
+        user: user._id,
+        comment: req.body.comment,
+        createdAt: Date.now(),
+      });
+      await articles.save();
+      res
+        .status(200)
+        .json({ status: "success", message: "Comment added successfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        status: "failed",
+        message: "Failed add comment",
+      });
+    }
+  },
+};
--- models/articles.js
+++ models/articles.js
@@ -1,37 +1,46 @@
 const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        trim: true,
-        required: [true, "Please add tittle"],
-    },
-    content:{
-        type: String,
-        trim: true,
-        required: [true, "please add content"],
-    },
-    image:{
-        type: String,
-        required: [true, "Please add image"] 
-    },
-    date:{
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add tittle"],
+  },
+  content: {
+    type: String,
+    trim: true,
+    required: [true, "please add content"],
+  },
+  image: {
+    type: String,
+    required: [true, "Please add image"],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: [true],
+  },
+  like: {
+    type: Array,
+    default: [],
+  },
+  dislike: {
+    type: Array,
+    default: [],
+  },
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      comment: String,
+      createdAt: {
         type: Date,
         default: Date.now,
-        required: [true],
+      },
     },
-    like:{
-        type: Array,
-        default: [],
-    },
-    dislike:{
-        type: Array,
-        default: [],
-    },
-    comment:{
-        type: Array,
-        default: [],
-    },
+  ],
 });
 
-module.exports= mongoose.model("articles", articleSchema);
+module.exports = mongoose.model("articles", articleSchema);
